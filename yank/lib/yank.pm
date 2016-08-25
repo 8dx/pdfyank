b18ce411e019cf2bace23b6cabf9904eedcb68e6
oz modic <syzygy.antithesis@gmail.com>
2016-08-25 02:12:05 -0500

    did some stuff, also things.

--- yank/lib/yank.pm
+++ yank/lib/yank.pm
@@ -1,10 +1,46 @@
 package yank;
 use Dancer ':syntax';
-
+use Data::Dumper;
+use Dancer::Plugin::WebSocket;
 our $VERSION = '0.1';
+use feature 'say';
 
 get '/' => sub {
     template 'index';
+};
+
+ws_on_message sub {
+	my $data = shift;
+	warn "i got a fucking message! ";
+	warn Dumper $data;
+	my $c = Dumper $data;
+	my $res = { 
+		  type => 'message',
+		  text =>  $c,
+		  time => time,
+		  respond => rand
+	 };
+        say "herez da ws_send object doe";
+	say Dumper $res;
+	say "sending now...";
+	ws_send $res;
+	ws_send {
+		type => 'rescue'
+	}
+};
+
+ws_on_new_listener sub {
+	ws_send { msg => "welcome to hell my nigga <3" };
+};
+
+any '/send_msg' => sub {
+        my $msg = params->{msg};
+        ws_send {
+		type => "message",
+		text => $msg,
+		time => time,
+		respond => 0
+	}
 };
 
 true;
--- yank/public/javascripts/main.js
+++ yank/public/javascripts/main.js
@@ -4,15 +4,34 @@ $(function() {
 	var host = loc[2];
 	var ws = "ws://"+host+"/_hippie/ws";
 	var socket = new WebSocket(ws);
+	window.ws = socket;
 	socket.onopen = function() {
             document.getElementById('conn-status').innerHTML = 'Connected';
+	    send_msg("hello there")
         };
         socket.onmessage = function(e) {
+	    alert("message")
+	    console.log("got message")
+	    console.info(e)
             var data = JSON.parse(e.data);
-            if (data.msg)
-		$("#messages").append($("<div class='msg'>"+data.msg+"</div>"));
+	    console.info(data)
+	    console.log('datamsg')
+	    console.info(data.msg)
+            if (data.msg) {
+		$("#messages").append($("<div class='msg'>"+JSON.stringify(data.msg)+"</div>"));
+		}
         };
-        function send_msg(message) {
-            socket.send(JSON.stringify({ msg: message }));
+
+        function send_msg(message,opts) {
+		console.log("sending message ", message);
+		var data = { type: "message", text: message, time: Date.now()};
+		$.extend(data,opts)
+		console.info(data)
+		socket.send(JSON.stringify(data))
         }
+	$("form").submit(function(e) {
+		e.preventDefault();
+		send_msg("i got this for data", { form: $("form").serialize() });
+		return false;
+	})
 })
